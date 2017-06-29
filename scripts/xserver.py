0e8c5075bc139164a5af6cf1c80fb1c6f732037c
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-06-29 16:45:46 +0200

    needed to remove virtualoutput symlink to be able to startx

--- scripts/xserver.py
+++ scripts/xserver.py
@@ -10,5 +10,5 @@ tools.pacaur(packages)
 
 #CONFIGURATION
 tools.link_conf('~/Projects/dotfiles/confs/xinitrc_conf', '~/.xinitrc')
-tools.link_conf('~/Projects/dotfiles/confs/virtualoutput_conf', '/etc/X11/xorg.conf.d/60-virtualoutput.conf', sudo=True)
+#tools.link_conf('~/Projects/dotfiles/confs/virtualoutput_conf', '/etc/X11/xorg.conf.d/60-virtualoutput.conf', sudo=True)
 
