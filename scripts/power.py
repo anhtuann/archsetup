3b1286bf212e48b6f29ba288dafc68d129ed462e
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-07-01 11:44:36 +0200

    caffeine added for screen blanking management

--- scripts/power.py
+++ scripts/power.py
@@ -1,13 +1,5 @@
 from scripts import tools
-import choices
 
 #INSTALL
-if choices.INSTALL_ENV == 'machine':
-    packages = ['tlp']
-
-#CONFIGURATION
-if choices.INSTALL_ENV == 'machine':
-    tools.bash_cmd(['sudo', 'systemctl', 'enable', 'tlp.service'])
-    tools.bash_cmd(['sudo', 'systemctl', 'enable', 'tlp-sleep.service'])
-    tools.bash_cmd(['sudo', 'systemctl', 'disable', 'systemd-rfkill.service'])
-    tools.link_conf('~/Projects/dotfiles/confs/tlp_conf', '/etc/default/tlp', sudo=True)
+packages = ['caffeine-ng']
+tools.pacaur(packages)
