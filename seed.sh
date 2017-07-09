9694f1b586678c997d95f2b3095a9b4f7bbeacdb
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-07-09 10:44:24 +0200

    added verbose to reflector for better output

--- seed.sh
+++ seed.sh
@@ -4,7 +4,7 @@
 sudo pacman -Sy --noconfirm wget reflector
 sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.old
 sudo wget https://www.archlinux.org/mirrorlist/all/ -O /etc/pacman.d/mirrorlist
-sudo reflector --protocol http --sort rate --fastest 6 --threads 10 --save /etc/pacman.d/mirrorlist
+sudo reflector --verbose --protocol http --sort rate --fastest 6 --threads 10 --save /etc/pacman.d/mirrorlist
 sudo pacman -Syu --noconfirm
 
 #Get dotfiles project
