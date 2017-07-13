e5a36b5c1e80a0146aba4d423c1ed466de7fdd17
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-07-13 09:33:19 +0200

    ubuntu-themes added to theming for caffeine icon

--- scripts/theming.py
+++ scripts/theming.py
@@ -1,5 +1,10 @@
 from scripts import tools
 
 #INSTALL
-packages = ['lxappearance']
+packages = ['lxappearance',
+            'ubuntu-themes']
 tools.pacaur(packages)
+
+#CONFIGURATION
+tools.mkdir('~/.config/gtk-3.0')
+tools.link_conf('~/Projects/dotfiles/confs/gtk3_ini', '~/.config/gtk-3.0/settings.ini')
