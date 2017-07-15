c19e7740207b1ec70cf56db1d9d2348a2a44e033
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-07-15 15:15:11 +0200

    deluge notifications added

--- scripts/web_tools.py
+++ scripts/web_tools.py
@@ -1,8 +1,13 @@
 from scripts import tools
 
+#INSTALL
 packages = ['dropbox',
             'deluge',
             'python2-notify',
             'python2-mako',
             'python2-service-identity']
 tools.pacaur(packages)
+
+#CONFIGURATION
+tools.mkdir('~/.config/deluge')
+tools.link_conf('~/Projects/dotfiles/confs/deluge_notifs_conf', '~/.config/deluge/notifications-gtk.conf')
