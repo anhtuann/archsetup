4b74982e885963495bf7d7ea95c40911777dbf94
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-07-16 22:53:22 +0200

    flake8 conf added for vim python syntax configuration

--- scripts/vim.py
+++ scripts/vim.py
@@ -11,7 +11,7 @@ tools.mkdir('~/.vim/bundle')
 with tools.cd('~/.vim/bundle'):
     tools.git_clone('https://github.com/VundleVim/Vundle.vim.git', 'Vundle.vim')
 tools.link_conf('~/Projects/dotfiles/confs/vimrc_conf', '~/.vimrc')
-tools.link_conf('~/Projects/dotfiles/confs/pep8_conf', '~/.config/pep8')
+tools.link_conf('~/Projects/dotfiles/confs/flake8_conf', '~/.config/flake8')
 command = 'vim -E -S ~/.vimrc +PluginInstall +qall'
 tools.bash_cmd(command, shell=True)
 with tools.cd('~/.vim/bundle/YouCompleteMe'):
