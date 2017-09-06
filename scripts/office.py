60808033164cb1b3b6961e04083e256fedc80b07
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-09-06 11:55:42 +0200

    zathura added for pdf reading (replacing firefox)

--- scripts/office.py
+++ scripts/office.py
@@ -1,5 +1,10 @@
 from scripts import tools
 
 #INSTALL
-packages = ['libreoffice']
+packages = ['libreoffice',
+            'zathura',
+            'zathura-pdf-mupdf']
 tools.pacaur(packages)
+
+#CONFIGURATION
+tools.link_conf('~/Projects/dotfiles/confs/zathura_conf', '~/.config/zathura/zathurarc')
