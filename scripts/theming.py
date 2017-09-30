5469f9d5ff78d7d6cb0b464f497f527a13a892b5
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-09-30 08:32:42 +0200

    qt5ct added to theme qt5 apps

--- scripts/theming.py
+++ scripts/theming.py
@@ -2,9 +2,11 @@ from scripts import tools
 
 #INSTALL
 packages = ['lxappearance',
-            'ubuntu-themes']
+            'ubuntu-themes',
+            'qt5ct']
 tools.pacaur(packages)
 
 #CONFIGURATION
 tools.mkdir('~/.config/gtk-3.0')
 tools.link_conf('~/Projects/dotfiles/confs/gtk3_ini', '~/.config/gtk-3.0/settings.ini')
+tools.link_conf('~/Projects/dotfiles/confs/qt5ct_conf', '~/.config/qt5ct/qt5ct.conf')
