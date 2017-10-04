5875a50d54d49f2e9c8043a061008e5dfb0dbd13
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-10-04 14:21:41 +0200

    vim install patched thanks to sandman commit 69b0c761c5052d3fb00fbd2d04cfe6472baaf885

--- scripts/vim.py
+++ scripts/vim.py
@@ -14,6 +14,11 @@ tools.link_conf('~/Projects/dotfiles/confs/vimrc_conf', '~/.vimrc')
 tools.link_conf('~/Projects/dotfiles/confs/flake8_conf', '~/.config/flake8')
 command = 'vim -E -S ~/.vimrc +PluginInstall +qall'
 tools.bash_cmd(command, shell=True)
+tools.mkdir('/tmp/ycm_build')
+with tools.cd('/tmp/ycm_build'):
+    tools.bash_cmd('cmake -G "Unix Makefiles" . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp', shell=True)
+    tools.bash_cmd('cmake --build . --target ycm_core', shell=True)
 with tools.cd('~/.vim/bundle/YouCompleteMe'):
+    tools.bash_cmd('git submodule update --init --recursive', shell=True)
     command = 'python install.py --system-boost'
     tools.bash_cmd(command.split(' '))
