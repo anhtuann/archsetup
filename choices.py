e33a4b48bb2de76d83dd1eec6656ad86ee89fd5b
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-10-04 15:09:08 +0200

    choices trimmed to fit sandman setup

--- choices.py
+++ choices.py
@@ -2,31 +2,12 @@ INSTALL_ENV = 'machine'
 #Real install would be 'machine'
 
 MINIMAL = ['pacaur.py',
-           'cli_tools.py',
-           'xserver.py',
-           'backlight.py',
            'i3wm.py',
-           'fs_management.py',
            'fonts.py',
-           'theming.py',
-           'sound.py',
-           'browser.py',
            'code_env.py',
            'web_dev.py',
-           'multimedia.py',
-           'web_tools.py',
-           'office.py',
            'vim.py',
            'shell.py']
 
-HARDWARE = ['power.py',
-            'sys_tools.py',
-            'input.py',
-            'network.py',
-            'bluetooth.py']
-
 STEPS = list()
 STEPS.extend(MINIMAL)
-
-if INSTALL_ENV == 'machine':
-    STEPS.extend(HARDWARE)
