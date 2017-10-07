02c2b361a07492c607842d7c8484c179bdfba9b8
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-10-07 16:13:59 +0100

    qutebrowser added in hope to replace firefox

--- scripts/browser.py
+++ scripts/browser.py
@@ -4,8 +4,10 @@ from scripts import tools
 packages = ['firefox',
             'gstreamer',
             'gst-libav', 
-            'gst-plugins-good']
+            'gst-plugins-good',
+            'qutebrowser']
 tools.pacaur(packages)
 
 #CONFIGURATION
 tools.link_conf('~/Projects/dotfiles/confs/vimperatorrc_conf', '~/.vimperatorrc')
+tools.link_conf('~/Projects/dotfiles/confs/qutebrowser_conf', '~/.config/qutebrowser/qutebrowser.conf')
