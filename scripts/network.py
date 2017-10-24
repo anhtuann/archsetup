d661173b35059752ddbcb55e089223382a69a1ba
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-10-24 09:46:55 +0200

    connman managed by stow too

--- scripts/network.py
+++ scripts/network.py
@@ -6,5 +6,4 @@ tools.pacaur(packages)
 
 #CONFIGURATION
 tools.bash_cmd("sudo sed -i '/FR/s/^#//g' /etc/conf.d/wireless-regdom")
-tools.mkdir('/etc/connman/', sudo=True)
-tools.bash_cmd('sudo cp ~/Projects/dotfiles/confs/connman_conf /etc/connman/main.conf')
+tools.stow('connman', sudo=True)
