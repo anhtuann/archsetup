5734d3e9e8ba8054a72448ccf46ffa56aa885eb8
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-10-27 09:50:17 +0000

    matrix server script added

---
+++ scripts/matrix.py
@@ -0,0 +1,17 @@
+from scripts import tools
+
+#INSTALL
+packages = ['matrix-synapse']
+tools.pacaur(packages)
+
+#CONFIGURATION
+tools.bash_cmd('sudo chmod 755 -R /var/lib/synapse')
+tools.bash_cmd('sudo usermod -aG synapse anhtuann')
+with tools.cd('/var/lib/synapse'):
+    command = ['sudo -u synapse python2 -m synapse.app.homeserver']
+    command.append('--server-name anhtuann.com')
+    command.append('--config-path /etc/synapse/homeserver.yaml')
+    command.append('--generate-config')
+    command.append('--report-stats=yes')
+    tools.bash_cmd(' '.join(command))
+tools.bash_cmd('sudo systemctl enable --now synapse.service')
