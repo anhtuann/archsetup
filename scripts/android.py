65ea8f8b9b750b7c2163d842be91e7676b3053e9
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-11-08 15:01:19 +0100

    android tools to communicate and flash my smartphone added

---
+++ scripts/android.py
@@ -0,0 +1,9 @@
+from scripts import tools
+
+#INSTALL
+packages = ['android-tools',
+            'android-udev']
+tools.pacaur(packages)
+
+#CONFIGURATION
+tools.bash_cmd('sudo usermod -aG adbusers anhtuann')
