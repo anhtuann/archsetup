8773bcdfa971c136cb0bcb4aadf37a2f9f4e8794
Anh Tuan Nguyen <anhtuann@protonmail.com>
2017-11-20 19:12:13 +0100

    virtualbox script updated for virtualbox 5.0

--- scripts/virtualbox.py
+++ scripts/virtualbox.py
@@ -7,17 +7,10 @@ if choices.INSTALL_ENV == 'virtualbox':
                 'virtualbox-guest-utils']
     tools.pacaur(packages)
 else:
-    packages = ['virtualbox', 
-                'virtualbox-host-dkms', 
-                'linux-headers',
+    packages = ['virtualbox',
+                'virtualbox-host-modules-arch',
                 'virtualbox-guest-iso',
                 'net-tools',
-                'virtualbox-ext-oracle',
-                'qt4']
+                'virtualbox-ext-oracle']
     tools.pacaur(packages)
-    tools.bash_cmd("sudo dkms install vboxhost/$(pacman -Q virtualbox|awk '{print $2}'|sed 's/\-.\+//') -k $(uname -rm|sed 's/\ /\//')")
     tools.bash_cmd("sudo gpasswd -a $USER vboxusers")
-    
-#CONFIGURATION
-if choices.INSTALL_ENV != 'virtualbox':
-    tools.stow('virtualbox', sudo=True)
