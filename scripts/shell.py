9bc24ea24986a41362db1b4726613002b85868bc
Anh Tuan Nguyen <anhtuann@protonmail.com>
2017-11-21 16:43:25 +0100

    zshrc overwritten by oh-my-zsh was conflicting with stow

--- scripts/shell.py
+++ scripts/shell.py
@@ -13,5 +13,6 @@ command = 'sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/to
 tools.bash_cmd('sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"')
 with tools.cd('~/.oh-my-zsh'):
     tools.bash_cmd('git apply ~/Projects/archsetup/confs/fix_window_title_tmux.patch')
+tools.bash_cmd('rm ~/.zshrc')
 tools.stow('zsh')
 tools.link_conf('~/.config/base16-shell/scripts/base16-flat.sh', '~/.base16_theme')
