845db801f04fee0d047790167104e96e10f4ff8e
Anh Tuan Nguyen <anhtuann@protonmail.com>
2017-11-22 16:48:58 +0000

    aria2 added to webtools

--- scripts/web_tools.py
+++ scripts/web_tools.py
@@ -2,5 +2,10 @@ from scripts import tools
 
 #INSTALL
 packages = ['deluge',
-            'python2-service-identity']
+            'python2-service-identity',
+            'aria2']
 tools.pacaur(packages)
+
+#CONFIGURATION
+tools.stow(aria2)
+tools.bash_cmd('touch ~/.aria2_input.conf')
