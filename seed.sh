84fce60710548685729c7523a6233e0813a46560
Anh Tuan Nguyen <anhtuann@protonmail.com>
2017-12-14 17:44:20 +0100

    --needed added to speed seed tests

--- seed.sh
+++ seed.sh
@@ -1,14 +1,14 @@
 #Update and optimize mirrorlist for pacman
 #Code taken from a linode stackscript and added sudo to it
 #https://www.linode.com/stackscripts/view/12580
-sudo pacman -Sy --noconfirm wget reflector
+sudo pacman -Sy --noconfirm --needed wget reflector
 sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.old
 sudo wget https://www.archlinux.org/mirrorlist/all/ -O /etc/pacman.d/mirrorlist
 sudo reflector --verbose --protocol http --sort rate --fastest 6 --threads 10 --save /etc/pacman.d/mirrorlist
 sudo pacman -Syu --noconfirm
 
 #Get archsetup and dotfiles project
-sudo pacman -S --noconfirm git openssh stow
+sudo pacman -S --noconfirm --needed git openssh stow
 mkdir ~/Projects
 git clone gitsandman:anhtuann/archsetup.git ~/Projects/archsetup
 git clone gitsandman:anhtuann/dotfiles.git ~/.dotfiles
