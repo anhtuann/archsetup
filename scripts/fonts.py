da4e2187036c45f60ad109f67fc890c44ac7bd18
Anh Tuan Nguyen <anhtuann@protonmail.com>
2018-03-13 12:17:41 +0100

    ttf-font-awesome-4 replacing its new version 5 which is not free anymore

--- scripts/fonts.py
+++ scripts/fonts.py
@@ -5,7 +5,7 @@ packages = ['ttf-hack-powerline-git',
             'ttf-symbola',
             'ttf-droid',
             'ttf-mplus',
-            'ttf-font-awesome',
+            'ttf-font-awesome-4',
             'ttf-unifont',
             'siji-git']
 tools.pacaur(packages)
