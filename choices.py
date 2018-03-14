05f2e0b18feaaedbe017f0bfab969150ef70b7af
Anh Tuan Nguyen <anhtuann@protonmail.com>
2018-03-14 10:20:42 +0100

    choices for zelda diverging from tardis

--- choices.py
+++ choices.py
@@ -2,22 +2,11 @@ INSTALL_ENV = 'machine'
 #Real install would be 'machine'
 
 MINIMAL = ['pacaur.py',
-           'cli_tools.py',
            'xserver.py',
            'backlight.py',
            'fonts.py',
-           'i3wm.py',
            'fs_management.py',
-           'theming.py',
            'sound.py',
-           'browser.py',
-           'code_env.py',
-           'multimedia.py',
-           'web_tools.py',
-           'office.py',
-           'virtualbox.py',
-           'syncthing.py',
-           'security.py',
            'vim.py',
            'shell.py']
 
@@ -25,10 +14,7 @@ HARDWARE = ['power.py',
             'battery.py',
             'sys_tools.py',
             'input.py',
-            'network.py',
-            'bluetooth.py',
-            'printer.py',
-            'scanner.py']
+            'network.py']
 
 CODING = ['web_dev.py',
           'tensorflow.py',
