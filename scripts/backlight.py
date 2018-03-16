6b10ac0acc2bfc24b8ee09845a05555fc737ffe2
Anh Tuan Nguyen <anhtuann@protonmail.com>
2018-03-16 10:18:29 +0100

    backlight fixed for zelda

--- scripts/backlight.py
+++ scripts/backlight.py
@@ -1,9 +1,11 @@
 from scripts import tools
 
 #INSTALL
-packages = ['xorg-xbacklight',
+packages = ['acpilight',
             'xf86-video-intel']
 tools.pacaur(packages)
 
 #CONFIGURATION
 tools.stow('backlight', sudo=True)
+tools.bash_cmd('sudo cp ~/Projects/archsetup/confs/backlight_rules /etc/udev/rules.d/90-backlight.rules')
+tools.bash_cmd('sudo usermod -aG video anhtuann')
