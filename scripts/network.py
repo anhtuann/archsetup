7a61df4b5f7478c18abfc6826a3777d1161e2c9d
Anh Tuan Nguyen <anhtuann@protonmail.com>
2018-03-19 11:34:20 +0100

    network-manager added to replace connman

--- scripts/network.py
+++ scripts/network.py
@@ -2,9 +2,11 @@ from scripts import tools
 
 #INSTALL
 packages = ['crda',
-            'nmap']
+            'nmap',
+            'networkmanager',
+            'network-manager-applet',
+            'gnome-keyring']
 tools.pacaur(packages)
 
 #CONFIGURATION
 tools.bash_cmd("sudo sed -i '/FR/s/^#//g' /etc/conf.d/wireless-regdom")
-tools.stow('connman', sudo=True)
