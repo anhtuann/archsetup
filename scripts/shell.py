0c51da3ebf76104f9135fda2ecc460f77ad9c5e7
Anh Tuan Nguyen <anhtuann@protonmail.com>
2018-04-09 09:46:45 +0200

    tmux fix for polybar is not needed in hogwarts

--- scripts/shell.py
+++ scripts/shell.py
@@ -12,8 +12,6 @@ with tools.cd('~/.config'):
     tools.git_clone('https://github.com/chriskempson/base16-shell.git')
 command = 'sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"'
 tools.bash_cmd('sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"')
-with tools.cd('~/.oh-my-zsh'):
-    tools.bash_cmd('git apply ~/Projects/archsetup/confs/fix_window_title_tmux.patch')
 tools.bash_cmd('rm ~/.zshrc')
 tools.stow('zsh')
 tools.link_conf('~/.config/base16-shell/scripts/base16-flat.sh', '~/.base16_theme')
