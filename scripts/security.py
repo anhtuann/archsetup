7baa347931b8af52579d562de73b9af3d278441d
Anh Tuan Nguyen <anhtuann@protonmail.com>
2018-04-24 17:45:37 +0200

    tor-browser added

--- scripts/security.py
+++ scripts/security.py
@@ -1,10 +1,12 @@
 from scripts import tools
 
 #INSTALL
+tools.bash_cmd('gpg --recv-keys D1483FA6C3C07136') #Preparation for tor-browser
 packages = ['keepassxc',
             'pass',
             'rofi-pass',
-            'gnupg']
+            'gnupg',
+            'tor-browser']
 tools.pacaur(packages)
 
 #CONFIGURATION
