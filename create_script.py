f5d0b34e59b637af7aa2b5b707927996bc284bc2
Anh Tuan Nguyen <anhtuann@protonmail.com>
2018-06-19 11:14:21 +0200

    create_script copied from tardis with minor change (pacman instead of pacaur)

---
+++ create_script.py
@@ -0,0 +1,23 @@
+import sys
+import re
+from scripts import tools
+
+script_name = sys.argv[-1]
+script_path = '~/Projects/archsetup/scripts/{}.py'.format(script_name)
+tools.bash_cmd('touch {}'.format(script_path))
+
+template = """
+from scripts import tools
+
+#INSTALL
+packages = []
+tools.pacman(packages)
+
+#CONFIGURATION
+tools.stow('package_name')
+"""
+
+template = re.sub("^\s+|\s+$", "", template, flags=re.UNICODE)
+
+tools.bash_cmd('echo "{}" > {}'.format(template, script_path))
+print('{}.py successfully created'.format(script_name))
